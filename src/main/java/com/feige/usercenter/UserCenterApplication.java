b478e6c5a0cb6a0b20fb86bb9ca79b253aaaea96
sqgxxx <13896628910@163.com>
2025-10-08 16:22:17 +0800

    Initial commit

---
+++ src/main/java/com/feige/usercenter/UserCenterApplication.java
@@ -0,0 +1,19 @@
+package com.feige.usercenter;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ */
+@SpringBootApplication
+@MapperScan("com.feige.usercenter.mapper")
+public class UserCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserCenterApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/feige/usercenter/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.feige.usercenter.mapper;
+
+import com.feige.usercenter.model.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 用户 Mapper
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
